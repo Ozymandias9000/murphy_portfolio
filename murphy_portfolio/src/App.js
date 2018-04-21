26cd63e36b074758b995a4587c8b2d76401ec9dd
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-04-21 15:08:57 -0700

    Added reponsive flex project gallery

--- murphy_portfolio/src/App.js
+++ murphy_portfolio/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/header';
 import AboutMe from './components/AboutMe/aboutme';
+import Projects from './components/Projects/projects';
 
 class App extends Component {
   render() {
+  	
     return (
       <div>
         <Header />
         <AboutMe />
+        <Projects />
       </div>
     );
   }
---
+++ murphy_portfolio/src/components/Projects/projects.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './projects.css';
+
+class Projects extends Component {
+	componentDidMount() {
+		const panels = document.querySelectorAll('.panel');
+
+		function toggleOpen(e) {
+			// if (e.path[0] === p) {
+			console.log(e.path[0]);
+		  this.classList.toggle('open');
+		// }
+		}
+
+		function toggleActive(e) {
+		  if (e.propertyName.includes('flex')) {
+		  this.classList.toggle('open-active');
+		  }
+		}
+
+		panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+		panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+	}
+
+	render() {
+		return (
+			<section>
+				<div className='project-text'>
+					<h3>
+						Projects
+					</h3>
+					<p>
+						Click to see details!
+					</p>
+				</div>
+
+				<div className="panels-container">
+			    <div className="panel panel1">
+			      <p>Hey</p>
+			      <p>Let's</p>
+			      <p>Dance</p>
+			    </div>
+			    <div className="panel panel2">
+			      <p>Give</p>
+			      <p>Take</p>
+			      <p>Receive</p>
+			    </div>
+			    <div className="panel panel3">
+			      <p>Experience</p>
+			      <p>It</p>
+			      <p>Today</p>
+			    </div>
+			    <div className="panel panel4">
+			      <p>Give</p>
+			      <p>All</p>
+			      <p>You can</p>
+			    </div>
+			    <div className="panel panel5">
+			      <p>Life</p>
+			      <p>In</p>
+			      <p>Motion</p>
+			    </div>
+			    <div className="panel panel6">
+			      <p>What</p>
+			      <p>Now</p>
+			      <p>?</p>
+			    </div>
+			  </div>
+			</section>
+		);
+	}
+}
+
+export default Projects;
