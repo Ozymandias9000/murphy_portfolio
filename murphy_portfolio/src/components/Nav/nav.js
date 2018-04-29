86c9a852fdfc32c8e7f664d0a694aa4e4160fd04
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-04-29 14:35:54 -0700

    Added hamburger menu for smaller screens - still need to fix to right of screen

--- murphy_portfolio/src/components/Nav/nav.js
+++ murphy_portfolio/src/components/Nav/nav.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import './nav.css';
 
-const Nav = props => (
-	<nav>
-		<a href='#home'>Home</a>
-		<span></span>
-		<a href='#about'>About</a>
-		<a href='#contact'>Contact</a>
-		<a href='#resume'>Resume</a>
-		<a href='#projects'>Projects</a>
-	</nav>
-);
+class Nav extends React.Component {
+
+	constructor(props) {
+    super(props);
+    this.state = {
+        active: false,
+    };
+  }
+
+	toggleMenu = (e) => {
+		console.log(e.target)
+		const currentState = this.state.active;
+    this.setState({ active: !currentState });	
+	}
+
+	render() {
+		return (
+			<nav>
+				<a href='#home' id='home'>Home</a>
+				<span></span>
+				<div className="collapsible-menu">
+					 	<input type="checkbox" id="menu" />
+			      <label htmlFor='menu' id='menuIcon' onClick={this.toggleMenu}></label>
+					<div className='menu-content'>	
+						<ul className={this.state.active ? 'menu-open': 'menu-closed'}>
+							<li><a href='#about'>About</a></li>
+							<li><a href='#contact'>Contact</a></li>
+							<li><a href='#resume'>Resume</a></li>
+							<li><a href='#projects'>Projects</a></li>
+						</ul>
+					</div> 
+				</div>	
+			</nav>
+			);
+		}
+};
 
 export default Nav;
