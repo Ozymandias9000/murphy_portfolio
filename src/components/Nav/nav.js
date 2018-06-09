6b4c775f8b5ac482d3519c56060b8b1c0b5b9d4f
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-06-09 14:44:49 -0700

    Fixed nav--responsive bug!

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import './nav.css';
+import React from "react";
+import "./nav.css";
 
 class Nav extends React.Component {
-
-	constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-        active: true,
+      active: true
     };
   }
 
-	 toggleMenu = (e) => {
-			this.setState(prevState => ({ active: !prevState.active }))
-			e.currentTarget.classList.toggle('change');
-		};
+  toggleMenu = e => {
+    this.setState(prevState => ({ active: !prevState.active }));
+    e.currentTarget.classList.toggle("change");
+  };
 
-	render() {
-		return (
-			<nav className={this.state.active ? 'nav' : 'nav--responsive'}>
-				<a href='#home' id='home'>Home</a>
-				<a href='#about'>About</a>
-				<a href='#contact'>Contact</a>
-				<a href='#resume'>Resume</a>
-				<a href='#projects'>Projects</a>
-					<div className='menuIcon' onClick={this.toggleMenu}>
-					  <div className="bar1"></div>
-					  <div className="bar2"></div>
-					  <div className="bar3"></div>
-					</div>
-			</nav>
-			);
-		}
-};
+  render() {
+    return (
+      <nav
+        className={
+          this.state.active || window.innerWidth > 700
+            ? "nav"
+            : "nav nav--responsive"
+        }
+      >
+        <a href="#home" id="home">
+          Home
+        </a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <a href="#resume">Resume</a>
+        <a href="#projects">Projects</a>
+        <div className="menuIcon" onClick={this.toggleMenu}>
+          <div className="bar1" />
+          <div className="bar2" />
+          <div className="bar3" />
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Nav;
