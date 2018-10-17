9f2a5a0c848dc9b41cdb54a23e1df5b4354585e5
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-10-17 15:33:48 -0700

    changed unicode atom symbol to img for android display

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,10 +4,12 @@ import "./footer.css";
 const Footer = () => (
   <footer>
     <p>
-      Developed in React{" "}
-      <span role="img" aria-label="react logo">
-        &#x269B;
-      </span>{" "}
+      Developed in React
+      {"  "}
+      <img
+        src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/square_256/react.png"
+        alt="react logo"
+      />{" "}
       | Nick Murphy
     </p>
   </footer>
